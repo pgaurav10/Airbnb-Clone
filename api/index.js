9e06ddc3943d4223b540fa93487184f40f136782
Gaurav <gauravpatil24121997@gmail.com>
2023-12-10 18:02:17 -0800

    Completed Registration Page working

---
+++ api/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
+const User = require('./models/users')
+
+require('dotenv').config()
+const bcryptSalt = bcrypt.genSaltSync(8);
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:5176',
+}));
+
+
+mongoose.connect(process.env.MONGO_URL);
+app.get('/test', (req, res)=> {
+    res.json('test ok');
+});
+
+app.post('/register', async (req, res) => {
+    const {name, email, password} = req.body;
+
+    try {
+        const userDoc = await User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptSalt),
+        });
+        res.json(userDoc);
+    } catch (e) {
+        res.status(422).json(e);
+    }
+
+})
+app.listen(4000);
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,17 @@ import {Routes, Route} from "react-router-dom"
 import IndexPage from "./pages/IndexPage.jsx"
 import LoginPage from "./pages/LoginPage.jsx"
 import Layout from "./Layout"
+import Register from "./pages/RegisterPager.jsx";
+import axios from "axios";
 
+axios.defaults.baseURL = 'http://localhost:4000';
 function App() {
   return (
     <Routes>
         <Route path="/" element={<Layout />}>
             <Route index element={<IndexPage/>} />
             <Route path="/login" element={<LoginPage/>} />
+            <Route path="/register" element={<Register/>} />
         </Route>
     </Routes>
   )
